0b54b0a91436de120afc29bb7125614f15e8993c
alexveryk <veryk.av@gmail.com>
2023-04-24 01:18:52 +0300

    Update Card.jsx

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,16 @@
-import { CardContainer } from './Card.styled';
+import { UserLogo } from 'components/UserLogo/UserLogo';
+import { CardContainer, ImgBg, Line, Logo } from './Card.styled';
 
 export const Card = () => {
-  return <CardContainer></CardContainer>;
+  const logo = require('../../images/vector.png');
+  const bgImg = require('../../images/picture.png');
+
+  return (
+    <CardContainer>
+      <Logo src={logo} alt={'Logo'} />
+      <ImgBg src={bgImg} alt={'backgroundImg'} />
+      <Line />
+      <UserLogo />
+    </CardContainer>
+  );
 };
