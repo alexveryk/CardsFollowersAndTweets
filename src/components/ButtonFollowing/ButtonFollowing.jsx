9d1e59d801fd0e90d2fd790307adea91404215ab
alexveryk <veryk.av@gmail.com>
2023-04-23 22:36:15 +0300

    Create ButtonFollowing.jsx

---
+++ src/components/ButtonFollowing/ButtonFollowing.jsx
@@ -0,0 +1,9 @@
+import { BtnFollowing } from './ButtonFollowing.styled';
+
+export const ButtonFollowing = () => {
+  return (
+    <div style={{ margin: '50px' }}>
+      <BtnFollowing>Following</BtnFollowing>
+    </div>
+  );
+};
