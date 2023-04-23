9f1f44df90f4db9e4ff950c291f1c105b0a55f46
alexveryk <veryk.av@gmail.com>
2023-04-24 01:19:02 +0300

    Update UserLogo.jsx

---
+++ src/components/UserLogo/UserLogo.jsx
@@ -0,0 +1,9 @@
+import { UserLogoContainer, UserImg } from './UserLogo.styled';
+
+export const UserLogo = () => {
+  return (
+    <UserLogoContainer>
+      <UserImg alt={'alt'} />
+    </UserLogoContainer>
+  );
+};
