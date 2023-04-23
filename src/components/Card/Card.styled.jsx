6596b3c66ac34bacf627c7b6878631fc35cfe317
alexveryk <veryk.av@gmail.com>
2023-04-24 01:18:54 +0300

    Update Card.styled.jsx

--- src/components/Card/Card.styled.jsx
+++ src/components/Card/Card.styled.jsx
@@ -1,10 +1,12 @@
 import styled from '@emotion/styled';
 
 export const CardContainer = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 380px;
   height: 460px;
-  left: 863px;
-  top: 168px;
 
   background: linear-gradient(
     114.99deg,
@@ -14,4 +16,25 @@ export const CardContainer = styled.div`
   );
   box-shadow: -2.5777px 6.87386px 20.6216px rgba(0, 0, 0, 0.23);
   border-radius: 20px;
+`;
+
+export const Line = styled.div`
+  width: 380px;
+  height: 8px;
+
+  background: #ebd8ff;
+  box-shadow: 0px 3.43693px 3.43693px rgba(0, 0, 0, 0.06),
+    inset 0px -1.71846px 3.43693px #ae7be3, inset 0px 3.43693px 2.5777px #fbf8ff;
+`;
+
+export const Logo = styled.img`
+  position: absolute;
+  left: 20px;
+  top: 20px;
+`;
+
+export const ImgBg = styled.img`
+  position: absolute;
+  left: 36px;
+  top: 28px;
 `;
