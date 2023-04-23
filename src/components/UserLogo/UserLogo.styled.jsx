94cd0a70d06f85e57b0ed50f503c7bd4649fa52f
alexveryk <veryk.av@gmail.com>
2023-04-24 01:19:04 +0300

    Create UserLogo.styled.jsx

---
+++ src/components/UserLogo/UserLogo.styled.jsx
@@ -0,0 +1,24 @@
+import styled from '@emotion/styled';
+
+export const UserLogoContainer = styled.div`
+  position: absolute;
+  z-index: ;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 80px;
+  height: 80px;
+
+  background: #ebd8ff;
+  box-shadow: 0px 4.39163px 4.39163px rgba(0, 0, 0, 0.06),
+    inset 0px -2.19582px 4.39163px #ae7be3,
+    inset 0px 4.39163px 3.29372px #fbf8ff;
+  border-radius: 86px;
+`;
+
+export const UserImg = styled.div`
+  width: 62px;
+  height: 62px;
+  border-radius: 86px;
+  background-color: black;
+`;
