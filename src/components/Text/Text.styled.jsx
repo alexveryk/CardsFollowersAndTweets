5bbb781507f23af667a173531f1a4f86077865a4
alexveryk <veryk.av@gmail.com>
2023-04-24 01:18:59 +0300

    Create Text.styled.jsx

---
+++ src/components/Text/Text.styled.jsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+export const Text = styled.div`
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 24px;
+  text-transform: uppercase;
+
+  color: #ebd8ff;
+`;
