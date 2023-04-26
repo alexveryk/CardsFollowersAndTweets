1b5f76bff7f1b13e08d28ada76e67a38db70d7fb
alexveryk <veryk.av@gmail.com>
2023-04-26 14:24:22 +0300

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,11 +1,11 @@
-import { StyledLink } from './Navigation.styled';
-import { NavLink } from 'react-router-dom';
+// import { StyledLink } from './Navigation.styled';
+// import { NavLink } from 'react-router-dom';
 
-export const Navigation = () => {
-  return (
-    <nav>
-      <StyledLink to="/">Home !</StyledLink>
-      <StyledLink to="/tweets">Tweets!</StyledLink>
-    </nav>
-  );
-};
+// export const Navigation = () => {
+//   return (
+//     <nav>
+//       <StyledLink to="/">Home !</StyledLink>
+//       <StyledLink to="/tweets">Tweets!</StyledLink>
+//     </nav>
+//   );
+// };
