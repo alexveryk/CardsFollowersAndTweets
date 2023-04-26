318fdadb645ee870f42718b569d320e27f416ee5
alexveryk <veryk.av@gmail.com>
2023-04-26 13:18:09 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,67 @@
+import { getUsers } from 'components/API/api';
+import { Card } from 'components/Card/Card';
+import { useEffect, useState } from 'react';
+import { List } from './Home.styled';
+import { ButtonLoadMore } from 'components/Buttons/ButtonLoadMore';
+import { Section } from './Home.styled';
+
 const Home = () => {
-  return <div>Home</div>;
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await getUsers(page);
+
+        if (!response) {
+          console.log('Not Users found! :(');
+        }
+
+        setUsers(prevState => [...prevState, ...response.data]);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [page]);
+
+  const handlePageChange = () => {
+    setPage(prevState => prevState + 1);
+  };
+
+  console.log(users);
+
+  return (
+    <div style={{ maxWidth: 1200, margin: '0 auto', padding: '0 16px' }}>
+      {loading ? (
+        <p>Loading</p>
+      ) : (
+        <Section>
+          <List>
+            {users.map(user => {
+              return (
+                <li key={user.id}>
+                  <Card
+                    user={user.user}
+                    tweets={user.tweets}
+                    followers={user.followers}
+                    avatar={user.avatar}
+                  />
+                </li>
+              );
+            })}
+          </List>
+          {users.length < 12 && <ButtonLoadMore onChange={handlePageChange} />}
+        </Section>
+      )}
+    </div>
+  );
 };
 
 export default Home;
