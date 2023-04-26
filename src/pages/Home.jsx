fead62ecca817f1673dfdf33f062277f804cbed0
alexveryk <veryk.av@gmail.com>
2023-04-26 13:58:56 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -35,8 +35,6 @@ const Home = () => {
     setPage(prevState => prevState + 1);
   };
 
-  console.log(users);
-
   return (
     <div style={{ maxWidth: 1200, margin: '0 auto', padding: '0 16px' }}>
       {loading ? (
