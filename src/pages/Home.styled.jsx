580b1038b4c76c9abb6bb559bbe6602113082270
alexveryk <veryk.av@gmail.com>
2023-04-26 13:18:11 +0300

    Create Home.styled.jsx

---
+++ src/pages/Home.styled.jsx
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled';
+
+export const List = styled.ul`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 16px;
+  list-style: none;
+  margin: 0;
+
+  padding: 0;
+`;
+
+export const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 24px;
+`;
