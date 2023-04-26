c351c093c2531cd622865a0ae0842f99543a6399
alexveryk <48521735+alexveryk@users.noreply.github.com>
2023-04-26 11:25:19 +0000

    Deploying to gh-pages from @ alexveryk/CardsFollowersAndTweets@8e1973694618723f6af273440ca0627392903af1 🚀

--- static/js/721.64feb224.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[721],{721:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"Tweets"})}}}]);
-//# sourceMappingURL=721.64feb224.chunk.js.map
