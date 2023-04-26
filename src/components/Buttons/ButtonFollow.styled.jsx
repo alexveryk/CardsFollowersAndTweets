32de022c4f7b0d904d920a9e5b1596385284123a
alexveryk <veryk.av@gmail.com>
2023-04-26 20:08:14 +0300

    Update ButtonFollow.styled.jsx

--- src/components/Buttons/ButtonFollow.styled.jsx
+++ src/components/Buttons/ButtonFollow.styled.jsx
@@ -9,7 +9,7 @@ export const BtnFollow = styled.button`
   gap: 6px;
   width: 196px;
   height: 50px;
-  background: #ebd8ff;
+  background: #5cd3a8;
   box-shadow: 0px 3.43693px 3.43693px rgba(0, 0, 0, 0.25);
   border-radius: 10px;
   border: none;
