b7f64e404100b2b429aacab336af1bec2fa66d2d
alexveryk <veryk.av@gmail.com>
2023-04-26 22:06:23 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -43,7 +43,7 @@ export const App = () => {
       {loading ? (
         <p>Loading...</p>
       ) : (
-        users.length < 12 && <ButtonLoadMore onChange={handlePageChange} />
+        users.length < 24 && <ButtonLoadMore onChange={handlePageChange} />
       )}
     </Container>
   );
