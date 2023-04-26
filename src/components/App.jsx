9cb5c6de09b0ddf5eada2dacec1dd8c2870765d8
alexveryk <veryk.av@gmail.com>
2023-04-26 13:17:40 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,7 @@
 import { lazy } from 'react';
-import { Route, Routes } from 'react-router-dom';
 
 const Home = lazy(() => import('../pages/Home'));
-const Tweets = lazy(() => import('../pages/Tweets'));
 
 export const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />{' '}
-      <Route path="/tweets" element={<Tweets />} />
-      <Route path="*" element={<Home />} />
-    </Routes>
-  );
+  return <Home />;
 };
