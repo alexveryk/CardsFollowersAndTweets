f16a0a8df03f493b249df054b6d89ba34a920567
alexveryk <veryk.av@gmail.com>
2023-04-26 22:06:27 +0300

    Delete AppBar.jsx

--- src/components/AppBar/AppBar.jsx
+++
@@ -1,9 +0,0 @@
-// import { Navigation } from '../Navigation/Navigation';
-
-// export const AppBar = () => {
-//   return (
-//     <header>
-//       <Navigation />
-//     </header>
-//   );
-// };
