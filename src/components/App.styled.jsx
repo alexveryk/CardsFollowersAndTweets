eac03c86255ffeb46433c4731e16d015192a5644
alexveryk <veryk.av@gmail.com>
2023-04-26 20:08:09 +0300

    Create App.styled.jsx

---
+++ src/components/App.styled.jsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 24px;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0 16px 24px 16px;
+`;
